f8f992ad424844fd81e0dbaa90e32e272e8b0ecd
Lukas Dizy <69392457+Kalusd@users.noreply.github.com>
2025-03-25 18:35:14 +0100

    Ajout d'un composant Jeu

---
+++ Projet/src/app/jeu/jeu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jeu',
+  standalone: false,
+  templateUrl: './jeu.component.html',
+  styleUrl: './jeu.component.scss'
+})
+export class JeuComponent {
+
+}
