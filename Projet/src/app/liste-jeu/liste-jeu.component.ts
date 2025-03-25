fecdc998f15b1dc2e2a117c75c53bdd590e69930
Lukas Dizy <69392457+Kalusd@users.noreply.github.com>
2025-03-25 18:35:39 +0100

    Ajout d'un composant ListeJeu

---
+++ Projet/src/app/liste-jeu/liste-jeu.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-jeu',
+  standalone: false,
+  templateUrl: './liste-jeu.component.html',
+  styleUrl: './liste-jeu.component.scss'
+})
+export class ListeJeuComponent {
+
+  this.listeJeu = 
+
+}
