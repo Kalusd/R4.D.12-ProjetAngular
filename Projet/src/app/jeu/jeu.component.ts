280cc53cf3e2561ed77f8c61c84b0b1cfbc22716
Lukas Dizy <69392457+Kalusd@users.noreply.github.com>
2025-03-26 11:38:23 +0100

    Modification du composant Jeu pour afficher les informations d'un jeu

--- Projet/src/app/jeu/jeu.component.ts
+++ Projet/src/app/jeu/jeu.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Jeu } from "../models/jeu.model";
+import { JeuxService } from '../services/jeux.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-jeu',
@@ -6,6 +9,21 @@ import { Component } from '@angular/core';
   templateUrl: './jeu.component.html',
   styleUrl: './jeu.component.scss'
 })
-export class JeuComponent {
+export class JeuComponent implements OnInit {
+  @Input() Jeu!: Jeu;
+  leJeu!: Jeu;
+  idJeu!: string;
+
+  constructor(private monServiceJeux: JeuxService, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+      this.idJeu = this.route.snapshot.params['id'];
+      if (this.idJeu !== undefined) {
+        this.monServiceJeux.getJeuById(+this.idJeu).subscribe(jeu => {this.leJeu = jeu});
+      }
+      else {
+        this.leJeu = this.Jeu
+      }
+  }
 
 }
