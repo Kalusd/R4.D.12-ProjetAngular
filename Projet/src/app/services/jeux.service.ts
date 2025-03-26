37362cb319786377730eb7a28259a787e41e740d
Lukas Dizy <69392457+Kalusd@users.noreply.github.com>
2025-03-26 09:43:07 +0100

    Import des dépendances nécessaires dans le service

--- Projet/src/app/services/jeux.service.ts
+++ Projet/src/app/services/jeux.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Jeu } from '../models/jeu.model';
 
 @Injectable({
   providedIn: 'root'
