1fbdcc720d55ee1611068cd3bb556cc1849d2ce7
Lukas Dizy <69392457+Kalusd@users.noreply.github.com>
2025-03-26 15:56:07 +0100

    Modification des routes

--- Projet/src/app/app-routing.module.ts
+++ Projet/src/app/app-routing.module.ts
@@ -2,16 +2,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { JeuComponent } from './jeu/jeu.component';
 import { ListeJeuComponent } from './liste-jeu/liste-jeu.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { ListeReservationComponent } from './liste-reservation/liste-reservation.component';
 
 const routes: Routes = [
   {
-    path: 'catalog',
+    path: 'catalogue-jeux',
     component: ListeJeuComponent
   },
   {
     path: 'jeu/:id',
     component: JeuComponent
-  }
+  },
+  {
+    path: 'catalogue-reservations',
+    component: ListeReservationComponent
+  },
+  {
+    path: 'reservation/:id',
+    component: ReservationComponent
+  },
+
 ];
 
 @NgModule({
