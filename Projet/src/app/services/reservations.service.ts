a50ead9a53adf72161aa880af75abb3e2a6c34e5
Lukas Dizy <69392457+Kalusd@users.noreply.github.com>
2025-03-26 12:26:19 +0100

    Ajout d'un service reservations

---
+++ Projet/src/app/services/reservations.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Reservation } from '../models/reservation.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReservationsService {
+
+  constructor(private http: HttpClient) { }
+
+  getReservations(): Observable<Reservation[]> {
+    return this.http.get<Reservation[]>('http://localhost:3000/reservations');
+  }
+
+  getReservationById(id: number): Observable<Reservation> {
+    return this.http.get<Reservation>('http://localhost:3000/reservations/' + id);
+  }
+}
