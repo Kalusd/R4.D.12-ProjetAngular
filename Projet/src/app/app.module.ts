fbd569d64aa709200e9469d9f24b039dad459ef8
Lukas Dizy <69392457+Kalusd@users.noreply.github.com>
2025-03-26 15:05:51 +0100

    Ajout d'un composant Reservation

--- Projet/src/app/app.module.ts
+++ Projet/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { JeuComponent } from './jeu/jeu.component';
 import { ListeJeuComponent } from './liste-jeu/liste-jeu.component';
+import { ReservationComponent } from './reservation/reservation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     JeuComponent,
-    ListeJeuComponent
+    ListeJeuComponent,
+    ReservationComponent
   ],
   imports: [
     BrowserModule,
