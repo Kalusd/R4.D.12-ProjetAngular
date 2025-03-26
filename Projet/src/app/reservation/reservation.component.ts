f56442744d74d043c1e192c8c47ca3f410c899c0
Lukas Dizy <69392457+Kalusd@users.noreply.github.com>
2025-03-26 15:57:27 +0100

    Ajout d'un composant Reservation

--- Projet/src/app/reservation/reservation.component.ts
+++ Projet/src/app/reservation/reservation.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Reservation } from "../models/reservation.model";
+import { ReservationsService } from '../services/reservations.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-reservation',
@@ -6,6 +9,20 @@ import { Component } from '@angular/core';
   templateUrl: './reservation.component.html',
   styleUrl: './reservation.component.scss'
 })
-export class ReservationComponent {
+export class ReservationComponent implements OnInit{
+  @Input() Reservation!: Reservation;
+  laReservation!: Reservation;
+  idReservation!: string;
 
+  constructor(private monServiceReservations: ReservationsService, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+      this.idReservation = this.route.snapshot.params['id'];
+      if (this.idReservation !== undefined) {
+        this.monServiceReservations.getReservationById(+this.idReservation).subscribe(reservation => {this.laReservation = reservation});
+      }
+      else {
+        this.laReservation = this.Reservation
+      }
+  }
 }
