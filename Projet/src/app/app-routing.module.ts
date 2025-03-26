4ba8e5fc1bbfbea1b9007f27c100d2ac22b5d09c
Lukas Dizy <69392457+Kalusd@users.noreply.github.com>
2025-03-26 11:33:12 +0100

    Mise en place du routage

--- Projet/src/app/app-routing.module.ts
+++ Projet/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JeuComponent } from './jeu/jeu.component';
+import { ListeJeuComponent } from './liste-jeu/liste-jeu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'catalog',
+    component: ListeJeuComponent
+  },
+  {
+    path: 'jeu/:id',
+    component: JeuComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
