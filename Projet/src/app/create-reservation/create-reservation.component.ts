db5dccadd4f30835e62773c7f3167b29b661a306
Lukas Dizy <69392457+Kalusd@users.noreply.github.com>
2025-04-03 18:37:45 +0200

    Ajout de la sélection du jeu et de la plateforme dynamiquement

--- Projet/src/app/create-reservation/create-reservation.component.ts
+++ Projet/src/app/create-reservation/create-reservation.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Reservation } from '../models/reservation.model';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ReservationsService } from '../services/reservations.service';
+import { JeuxService } from '../services/jeux.service';
 import { Router } from '@angular/router';
+import { Jeu } from '../models/jeu.model';
 
 @Component({
   selector: 'app-create-reservation',
@@ -10,42 +12,49 @@ import { Router } from '@angular/router';
   templateUrl: './create-reservation.component.html',
   styleUrl: './create-reservation.component.scss'
 })
-export class CreateReservationComponent {
+export class CreateReservationComponent implements OnInit {
   leFormulaire!: FormGroup;
   reservationActuelle!: Reservation;
-  regexNumeroTelephone = new RegExp(/^\d{10}$/);
+  listeJeux!: Jeu[];
+  plateformesDisponibles!: string[]
 
-  constructor(private formBuilder: FormBuilder, private monServiceReservations: ReservationsService, private router: Router) {}
+  constructor(private formBuilder: FormBuilder, private monServiceReservations: ReservationsService, private monServiceJeux: JeuxService, private router: Router) {}
 
   ngOnInit(): void {
     // Initialisation du formulaire (on récupérera id plus tard, et idJeuReservation à travers le titre)
     this.leFormulaire = this.formBuilder.group({
       nomClient: [null, [Validators.required, Validators.minLength(3)]],
       emailClient: [null, [Validators.required, Validators.email]],
-      numeroTelephone: [null, [Validators.required, Validators.pattern(this.regexNumeroTelephone)]],
+      numeroTelephone: [null, [Validators.required, Validators.pattern(/^\d{10}$/)]],
       titreJeuReservation: [null, [Validators.required]],
       plateforme: [null, [Validators.required]],
       dateDeReservation: [null, [Validators.required]],
       statutReservation: [null, [Validators.required]]
     });
 
-    this.leFormulaire.valueChanges.subscribe((formValue) => {
-      this.reservationActuelle = {
-        id: 0,
-        nomClient: formValue.nomClient,
-        emailClient: formValue.emailClient,
-        numeroTelephone: formValue.numeroTelephone,
-        idJeuReservation: 0,
-        titreJeuReservation: formValue.titreJeuReservation,
-        plateforme: formValue.plateforme,
-        dateDeReservation: formValue.dateDeReservation,
-        statutReservation: formValue.statutReservation
-      };
-    });
+    // Récupération des jeux
+    this.monServiceJeux.getJeux().subscribe((jeux) => {
+      this.listeJeux = jeux;
+    })
+  }
+
+  onJeuChange(event: Event): void {
+    const titreJeu = (event.target as HTMLSelectElement).value;
+    // Récupération du jeu choisi
+    const jeuChoisi = this.listeJeux.find(jeu => jeu.titre === titreJeu);
+    if (jeuChoisi) {
+      // Mise à jour des plateformes disponibles en fonction du jeu choisi
+      this.plateformesDisponibles = Array.isArray(jeuChoisi.plateforme) ? jeuChoisi.plateforme : [jeuChoisi.plateforme];
+    }
+    else {
+      // Suppression des plateformes disponibles si aucun jeu n'est trouvé
+      this.plateformesDisponibles = [];
+    }
   }
 
   createReservation() {
     if (this.leFormulaire.valid) {
+      // Création d'une nouvelle réservation à partir du formulaire
       let newReservation = {
         id: 0,
         nomClient: this.leFormulaire.get('nomClient')?.value,
@@ -57,6 +66,12 @@ export class CreateReservationComponent {
         dateDeReservation: this.leFormulaire.get('dateDeReservation')?.value,
         statutReservation: this.leFormulaire.get('statutReservation')?.value
       };
+
+      // Récupération de l'identifiant du jeu à partir du titre
+      const jeuChoisi = this.listeJeux.find(jeu => jeu.titre === newReservation.titreJeuReservation);
+      if (jeuChoisi) {
+        newReservation.idJeuReservation = jeuChoisi.id;
+      }
 
       this.monServiceReservations.addReservation(newReservation).subscribe({
         next: (reservation: Reservation) => {
--- Projet/src/app/services/reservations.service.ts
+++ Projet/src/app/services/reservations.service.ts
@@ -1,16 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, switchMap, first } from 'rxjs';
+import { Observable, switchMap } from 'rxjs';
 import { Reservation } from '../models/reservation.model';
-import { JeuxService } from './jeux.service';
-import { Jeu } from '../models/jeu.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationsService {
 
-  constructor(private http: HttpClient, private monServiceJeux: JeuxService) { }
+  constructor(private http: HttpClient) { }
 
   getReservations(): Observable<Reservation[]> {
     return this.http.get<Reservation[]>('http://localhost:3000/reservations');
@@ -25,22 +23,8 @@ export class ReservationsService {
   }
 
   addReservation(newReservation: Reservation): Observable<Reservation> {
-    // this.monServiceJeux.getJeuByTitre(newReservation.titreJeuReservation).subscribe((jeux) => {
-    //   newReservation.idJeuReservation = jeux.id;
-    //   console.log('Jeu trouvé : ', jeu);
-    //   console.log('Nouvel id : ', newReservation.idJeuReservation);
-    // });
-    // Incrémentation de l'identifiant puis ajout en base de données
     return this.getReservations().pipe(
       switchMap((reservations: Reservation[]) => {
-        // Récupération de l'identifiant du jeu réservé
-        // this.monServiceJeux.getJeuByTitre(newReservation.titreJeuReservation).pipe(
-        //   switchMap((jeux: Jeu[]) => {
-        //     let idJeu: number = 0;
-        //     jeux.forEach((jeu: Jeu) => { idJeu = jeu.id; });
-        //     newReservation.idJeuReservation = idJeu;
-        //   })
-        // );
         let idMax: number = 0;
         reservations.forEach((reservation: Reservation) => { idMax = (reservation.id > idMax ? reservation.id : idMax); });
         newReservation.id = Number(idMax) + 1;
