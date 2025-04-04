663bf2ee4cea88db91b4e2945b619d359500e685
Lukas Dizy <69392457+Kalusd@users.noreply.github.com>
2025-04-05 01:06:11 +0200

    Filtrage des jeux sur le nom + plateforme et genre

--- Projet/src/app/liste-jeu/liste-jeu.component.ts
+++ Projet/src/app/liste-jeu/liste-jeu.component.ts
@@ -11,11 +11,45 @@ import { JeuxService } from '../services/jeux.service';
 export class ListeJeuComponent implements OnInit {
 
   listeJeu! : Jeu[];
+  listeJeuFiltree! : Jeu[];
+  listePlateformes! : string[];
+  listeGenres! : string[];
+  termeRecherche: string = '';
+  filtrePlateforme: string = '';
+  filtreGenre: string = '';
 
   constructor(private monServiceJeux: JeuxService) {}
 
   ngOnInit(): void {
-    this.monServiceJeux.getJeux().subscribe((jeux) => {this.listeJeu = jeux;});
-  } 
+    this.monServiceJeux.getJeux().subscribe((jeux) => {
+      // Récupération de la liste des jeux
+      this.listeJeu = jeux;
+      this.listeJeuFiltree = jeux;
+
+      // Récupération des plateformes et des genres uniques
+      this.listePlateformes = Array.from(
+        new Set(jeux.flatMap((jeu) => (Array.isArray(jeu.plateforme) ? jeu.plateforme : [jeu.plateforme])))
+      );
+      this.listeGenres = Array.from(new Set(jeux.map((jeu) => jeu.genre)));
+    });
+  }
+
+  appliquerFiltrage(): void {
+    this.listeJeuFiltree = this.listeJeu.filter((jeu) => {
+      const resultatsRecherche = this.termeRecherche
+        ? jeu.titre.toLowerCase().includes(this.termeRecherche.toLowerCase())
+        : true;
+
+      const resultatsFiltragePlateforme = this.filtrePlateforme
+        ? (Array.isArray(jeu.plateforme) ? jeu.plateforme : [jeu.plateforme]).includes(this.filtrePlateforme)
+        : true;
+
+      const resultatsFiltrageGenre = this.filtreGenre
+        ? jeu.genre === this.filtreGenre
+        : true;
+
+      return resultatsRecherche && resultatsFiltragePlateforme && resultatsFiltrageGenre;
+    });
+  }
 
 }
