f1933282230c7942a3b96899c0ac7a7d8325fb0b
Lukas Dizy <69392457+Kalusd@users.noreply.github.com>
2025-04-05 12:31:46 +0200

    Ajout d'une barre de navigation

--- Projet/src/app/app.module.ts
+++ Projet/src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 import { JeuComponent } from './jeu/jeu.component';
 import { ListeJeuComponent } from './liste-jeu/liste-jeu.component';
 import { ReservationComponent } from './reservation/reservation.component';
 import { ListeReservationComponent } from './liste-reservation/liste-reservation.component';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { CreateJeuComponent } from './create-jeu/create-jeu.component';
 import { CreateReservationComponent } from './create-reservation/create-reservation.component';
 
@@ -27,7 +30,10 @@ import { CreateReservationComponent } from './create-reservation/create-reservat
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
